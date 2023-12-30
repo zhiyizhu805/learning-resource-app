66906b76fb964822201670000f3c9a6eb06cd470
zhiyizhu805 <zhiyi.zhu@lincolnuni.ac.nz>
2023-12-31 08:11:26 +1300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul></ul>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            storedResources: [{
+                id: 'official-guide',
+                title: 'Official Guide',
+                description: 'The Official Guide to Vue.js',
+                link: 'https://vuejs.org/v2/guide/'
+            },
+            {
+                id: 'google',
+                title: 'Google',
+                description: 'Google Search Engine',
+                link: 'https://google.com'}
+            ]
+        }
+    },
+    }
+</script>
